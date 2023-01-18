a432d0b1ba3aba1c1fcfcf3883f9ae150f8d7749
ManiTej31 <manitejaaithu@gmail.com>
2023-01-18 20:58:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import {OnInit} from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'random-card';
+export class AppComponent implements OnInit {
+  title = 'random-cards';
+
+  user: any;
+
+  constructor( 
+    private userService: UserService, 
+    private toastr: ToastrService,
+    ) {}
+
+
+// on init of the app getting user and setting it to the user property and showing it
+  ngOnInit(): void {
+    console.log(this.user);
+
+    // using service to get user and set it
+    this.userService.getUser().subscribe(
+      (user: any) => {
+        console.log(user.results[0]);
+        this.user = user.results[0];
+      },
+      (err) => {
+        // handling error here
+        console.error(err);
+        this.toastr.error(err.status, 'Something went wrong');
+      }
+    );
+  }
 }
